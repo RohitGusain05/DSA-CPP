43ed0707ffd2d6403739facada5ee4bc36040170
RohitGusain05 <142705369+RohitGusain05@users.noreply.github.com>
2026-08-26 16:55:39 +0530

    Add second maximum solution

---
+++ Arrays/second_maximum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    int mx = INT_MIN, second = INT_MIN;
+    for(int x : a){
+        if(x > mx){
+            second = mx;
+            mx = x;
+        }
+        else if(x > second && x != mx){
+            second = x;
+        }
+    }
+
+    cout << second;
+}
